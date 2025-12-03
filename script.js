cece8e15f5b01720df2efc3621ae56e2af333519
Beth Parham <bethparham.49@gmail.com>
2025-12-03 15:36:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var listItems = document.querySelectorAll("li")
 
+
+
+//Add Items to List
 function inputLength() {
 	return input.value.length;
 }
@@ -11,6 +15,14 @@ function createListElement() {
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
+	// Delete Button
+    var deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class", "delete")
+    deleteButton.appendChild(document.createTextNode(" Delete "));
+    ul>li.appendChild(deleteButton).addEventListener("click", removeItem);
+
+    // Strike-through
+    ul.appendChild(li).addEventListener("click", toggleList);
 }
 
 function addListAfterClick() {
@@ -24,6 +36,35 @@ function addListAfterKeypress(event) {
 		createListElement();
 	}
 }
+
+function removeItem() {
+    this.parentNode.remove();
+}
+
+function toggleList () {
+    this.classList.toggle("done");
+}
+
+//Strikethrough
+var list = document.querySelectorAll('ul>li');
+for (var i = 0; i < list.length; i++) {
+list[i].addEventListener("click", toggleList);
+}
+function toggleList() {
+  event.target.classList.toggle("done"); //or this.classList.toggle("done");
+}
+
+                      
+//Delete List Items
+var elements = document.getElementsByClassName("delete");
+for (var i = 0; i < elements.length; i++){
+  elements[i].addEventListener("click", removeItem);
+}
+  
+function removeItem(){
+this.parentNode.remove();
+}  
+
 
 button.addEventListener("click", addListAfterClick);
 
